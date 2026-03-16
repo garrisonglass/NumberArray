fa7751e848ca7b19eb69cc3cd5be51626e1850d5
garrisonglass <garrison.glass@gmail.com>
2026-03-16 16:45:25 -0400

    Added function to .h without comments

--- NumberArray/NumberArray.h
+++ NumberArray/NumberArray.h
@@ -1,5 +1,26 @@
 #pragma once
+
+#include <iostream>
+using namespace std;
+
 class NumberArray
+
 {
+private:
+	int size;
+	double* data;
+
+	static const int MAX_SIZE = 100;
+
+public:
+	NumberArray(int size = MAX_SIZE);
+	~NumberArray();
+	void setNumber(int index, double value);
+	double getNumber(int index) const;
+	double getMin() const;
+	double getMax() const;
+	double getAvg() const;
+	void print() const;
+	int getSize() const { return size; }
 };
 
